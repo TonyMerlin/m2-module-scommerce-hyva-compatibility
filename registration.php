8004d474d910e18ff16cecdee9a713e95aaf5550
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2026-08-10 23:38:14 +0100

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,8 @@
+<?php
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Merlin_ScommerceHyvaCompatibility',
+    __DIR__
+);
